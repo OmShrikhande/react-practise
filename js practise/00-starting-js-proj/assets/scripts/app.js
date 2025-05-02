8e4661539e030acacb32146cc46c6da99424900a
jis24 <165779138+orscoder@users.noreply.github.com>
2025-05-02 15:21:01 +0530

    basics js  done

--- js practise/00-starting-js-proj/assets/scripts/app.js
+++ js practise/00-starting-js-proj/assets/scripts/app.js
@@ -1,4 +1,154 @@
+
+
 import { apiKey } from "./util.js";
 
 console.log(apiKey);
+/*
 
+
+// basic javascript code with the object 
+const userName = "Khan";
+const userAge = 30;
+const user = {
+    name: userName,
+    age: userAge,
+    greet() {
+        console.log("Hello " + this.name);
+    },
+}
+console.log(user.greet());
+*/
+
+
+
+
+
+/*
+//OOPs in javascript 
+class Demosh{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet() {
+        console.log("Hello " + this.name);
+    }
+}
+const demo = new Demosh("Khan", 30);
+demo.greet()
+*/
+
+
+
+
+
+
+
+/*
+//Aray and Array methods like map, filter, reduce, find, forEach, some, every, splice, slice, push, pop, shift, unshift
+
+const hobbies = ["sports", "cooking", "reading"];
+console.log(hobbies[0])
+
+hobbies.push("coding")
+
+const index = hobbies.findIndex((item)=>{
+    return item === "reading";
+})
+console.log(index)
+
+
+const logging = hobbies.map((item)=> ({text: item}) );
+console.log(logging)
+*/
+
+
+
+
+
+
+/*
+// destructuring of array and object
+
+const hobbies = ["sports", "cooking", "reading"];
+const user={
+    name: "Khan",
+    age: 30,
+}
+
+//destructureing of array
+const [hobby1, hobby2] = hobbies;
+//the upside code have the same meaning as the below code
+//alternative way to destructure the array
+// const hobby1 = hobbies[0];
+// const hobby2 = hobbies[1];
+
+console.log(hobby1)
+console.log(hobby2)
+
+//destructureing of object
+const {name:userName, age:Userage} = user; //here :userName and Userage are the new variable names (aliase)
+//the upside code have the same meaning as the below code   
+//alternative way to destructure the object
+// const name = user.name;
+// const age = user.age;
+console.log(userName)
+console.log(Userage)
+*/
+
+
+
+
+/*
+// Spread Operator 
+
+const user={
+    name: "Khan",
+    age: 30,
+}
+const hobbies = ["sports", "cooking", "reading"];
+
+//spread operator on the array
+const newHobbies = [...hobbies, "coding"];
+//spread operator spreads the array and adds the new element to the end of the array
+console.log(newHobbies)
+
+
+//spread operator on the object
+const extendedUser = {
+    isAdmin: true,
+    ...user,
+    //this will add the user object to the extendedUser object
+};
+console.log(extendedUser)
+*/
+
+
+
+
+
+
+
+
+
+/*
+// Conditional staments
+const password= prompt("Enter your password: ");
+if(password =="om"){
+console.log("Hello " + password);
+}else if(password == "neha"){
+    console.log("Hello " + password);
+}
+else{
+    console.log("Hello " + password);
+}
+*/
+
+
+/*
+//  for loop
+const numbers = [1, 2, 3, 4, 5];
+for(const number of numbers){
+    console.log(number);
+}
+*/
--- js practise/00-starting-js-proj/assets/scripts/util.js
+++ js practise/00-starting-js-proj/assets/scripts/util.js
@@ -1,1 +1,74 @@
-export let apiKey = "myname is khan";
+export const apiKey = "1234567890";
+
+
+
+// some tricky codes 
+
+
+
+
+
+/*
+//function as a value to inbuild functions
+function handleTimeout(){
+    console.log("Timeout!");
+}
+
+const handleTimeout2 = ()=> { 
+    console.log("Timeout ..agian!");
+}
+
+setTimeout(handleTimeout, 2000); //in this case the function is passed as a value to the setTimeout function and it will be executed after 2 seconds
+
+// if we pass the handelTimeout() instead of handleTimeout, it will be executed immediately and not after 2 seconds because in this case we are calling the function and not passing it as a value to the setTimeout function
+
+setTimeout(handleTimeout2, 3000); //in this case the function is passed as a value to the setTimeout function and it will be executed
+
+setTimeout(()=>{ //here we are passing an anonymous function to the setTimeout function and it will be executed but not immediately because we are defining it not executing it.
+    console.log("more Timeout ..agian!")
+}, 4000); //in this case the function is passed as a value to the setTimeout function and it will be executed
+*/
+
+
+
+/*
+//funtion as a parameter to another function
+function greeter(greetfn){
+    greetfn();
+}
+greeter(()=>{
+    console.log("Hello World!");
+})
+//in this case we are passing an anonymous function to the greeter function and it will be executed but not immediately because we are defining it not executing it.
+*/
+
+
+
+
+/*
+// function in a function
+function inti(){
+    function greet(){
+        console.log("Hello World!");
+    }
+    greet();
+}
+//greet(); //this will give an error because the greet function is not defined in the global scope`
+inti();
+*/
+
+
+
+
+
+
+
+
+//reference vs premitive types
+ const a = 10; //primitive type
+
+const hobbies = ["sports", "cooking", "reading"]; //reference type
+hobbies.push("coding"); //this will not change the reference of the hobbies array but will change the value of the array
+console.log(hobbies); //this will print the array with the new value
+
+
